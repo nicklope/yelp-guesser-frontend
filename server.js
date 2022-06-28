1d66dc914ffb226220b94806add0ffdf344bcb64
Nick Lopez <nicklopez5454@yahoo.com>
2022-06-28 18:17:01 -0500

    yelp server running

--- server.js
+++ server.js
@@ -12,4 +12,41 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////// YELP SERVER //////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+let API_KEY =
+  'aIgcPCstsSguvhNOM9KNSDbiwoqJK5Kv3TiEyjikYYBquOInYN4nA05Sb6S1akz3AUB23FVsevExkFxcgCZezklG2ECx6RhK0VBWH-htAy6SdHBziL_SQGZLiHdYYnYx'
+axios.defaults.headers.common = {
+  Authorization: `Bearer ${API_KEY}`
+}
+
+const getBusiness = async (zipCode) => {
+  let result = await axios.get(
+    `https://api.yelp.com/v3/businesses/search?location=${zipCode}&limit=50&offset=200&sort_by=rating`
+  )
+  return result.data.businesses
+}
+const getBusinessByReviews = async (id) => {
+  let result = await axios.get(
+    `https://api.yelp.com/v3/businesses/${id}/reviews`
+  )
+  return result.data
+}
+
+app.get('/businesses/:zipcode', async (req, res) => {
+  const { zipcode } = req.params
+  let businesses = await getBusiness(zipcode)
+
+  return res.status(200).json({ businesses })
+})
+app.get('/businessreviews/:id', async (req, res) => {
+  const { id } = req.params
+  const reviews = await getBusinessByReviews(id)
+  return res.status(200).json({ reviews })
+})
+
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
