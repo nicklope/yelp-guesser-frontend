239b35404d1d7e87ebc777c6c5e6775d820b7ef0
Nick Lopez <nicklopez5454@yahoo.com>
2022-08-01 10:33:38 -0500

    misc

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import SelectGame from './pages/SelectGame'
 import QuickPlay from './pages/QuickPlay'
+import Login from './pages/Login'
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/selectgame" element={<SelectGame />} />
         <Route path="/quickplay" element={<QuickPlay />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
---
+++ client/src/pages/Login.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react'
+const Login = () => {
+  return (
+    <div id="login-page">
+      <div id="login-form"></div>
+    </div>
+  )
+}
+export default Login
